97db4b1335093c739f910272ab982fab58dc443d
kotaro <kota1204996700@gmail.com>
2026-03-10 21:09:43 +0900

    Github Pages 初期設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-// tailwind のインポートを外す
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  // ユーザー名を自分のGitHubユーザー名に変更してください
+  site: "https://あなたのユーザー名.github.io",
+  // リポジトリ名に合わせて設定します
+  base: "/StudentAssociationWebsite",
+  integrations: [tailwind(), react()],
 });
